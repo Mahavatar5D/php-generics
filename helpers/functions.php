73dbde9bcf3a7a851fe910dcf28b80fdb49662d9
Mahavatar5D <mahavatar5d@gmail.com>
2022-12-13 00:06:36 -0300

    Add class-string psalm code quality test

--- helpers/functions.php
+++ helpers/functions.php
@@ -9,3 +9,13 @@ function array_first(array $list)
 {
     return array_shift($list);
 }
+
+/**
+ * @param class-string $s
+ *
+ * @return class-string
+ */
+function takesClassName(string $s): string
+{
+    return $s;
+}
--- tests/HelpersFunctionsTest.php
+++ tests/HelpersFunctionsTest.php
@@ -2,13 +2,20 @@
 
 namespace App\Tests;
 
+use App\A;
 use PHPUnit\Framework\TestCase;
 
-/** @psalm-suppress PropertyNotSetInConstructor **/
 class HelpersFunctionsTest extends TestCase
 {
     public function testArrayFirst(): void
     {
         $this->assertEquals(1, array_first([1,2,3]));
+    }
+
+    public function testtakesClassName(): void
+    {
+        $r2 = takesClassName(A::class);
+
+        $this->assertEquals("App\A", $r2);
     }
 }
